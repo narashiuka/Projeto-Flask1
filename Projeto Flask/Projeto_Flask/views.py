7c4651f4c110e83da41b7fc4da9e3c47f5bdf377
Gabriel Roberto Rodrigues Faria <gabriel.roberto.cep@gmail.com>
2020-06-09 06:28:14 -0300

    Criada tabela Tarefas, incluido regras de modificação e finalizada a tabela desenvolvedores

--- Projeto Flask/Projeto_Flask/views.py
+++ Projeto Flask/Projeto_Flask/views.py
@@ -39,8 +39,8 @@ def about():
 
 @app.route('/pessoa')
 def pessoa():
-    pesoy = jsonify({'id':1, 'nome':'Gabriel Roberto', 'profissao':'Engenheiro', 'idade':23, 'jogopreferido':'Minecraft'})
-    return render_template(pesoy.nome)
+    pesoy = {'id':1, 'nome':'Gabriel Roberto', 'profissao':'Engenheiro', 'idade':23, 'jogopreferido':'Minecraft'}
+    return jsonify(pesoy)
 
 #@app.route('/soma/<int:a>/<int:b>')
 #def somando(a, b):
@@ -55,3 +55,115 @@ def somando():
     elif (request.method == 'GET'):
         total = 10 + 10
     return jsonify({'soma':total})
+
+desenvolvedores = [
+    {'id':0,
+     'nome':'Igor',
+     'habilidade':['Python', 'R']},
+    {'id':1,
+     'nome':'Gabriel',
+     'habilidade':['Python', 'Javascript', 'Java']},
+    {'id':2,
+     'nome':'Pablo',
+     'habilidade':['R', 'HTML', 'CSS']}
+    ]
+
+#Devolve um desenvolvedor pelo ID, também consegue modificar os dados dos desenvolvedores
+@app.route('/dev/<int:id>/', methods=['GET', 'PUT', 'DELETE'])
+def desenvolvedor(id):
+
+    if request.method == 'GET':
+        try:
+            response = desenvolvedores[id]
+            print(desenvolvedor)
+        except IndexError:
+            mensagem = f'Registro {id} não existe'
+            response = {'status':'Falha', 'mensagem':mensagem}
+        except Exception:
+            mensagem = 'Erro Descconhecido, procure o administrador da API'
+            response = {'status':'Erro', 'mensagem':mensagem}
+        return jsonify(response)
+
+    elif request.method == 'PUT':
+        dados = json.loads(request.data)
+        desenvolvedores[id] = dados
+        return jsonify(dados)
+
+    elif request.method == 'DELETE':
+        desenvolvedores.pop(id)
+        return jsonify({'status':'Sucesso', 'mensagem':'Registro excluído com sucesso'})
+
+
+#Lista de todos desenvolvedores e permite incluir um novo desenvolvedor
+@app.route('/dev/', methods=['POST', 'GET'])
+def lista_desenvolvedores():
+    if request.method == 'POST':
+        dados = json.loads(request.data)
+        posicao = len(desenvolvedores)
+        dados['id'] = posicao
+        desenvolvedores.append(dados)
+        return jsonify(desenvolvedores[posicao])
+    elif request.method == 'GET':
+         return jsonify(desenvolvedores)
+
+
+tarefas = [
+    {'id':0,
+     'responsavel':'Stefani',
+     'tarefa':['Limpar', 'Dobrar'],
+     'status':'Em Progresso'},
+    {'id':1,
+     'responsavel':'Gabriel',
+     'tarefa':['Limpar', 'Passar', 'Lavar'],
+     'status':'Concluida'},
+    {'id':2,
+     'responsavel':'Henrique',
+     'tarefa':['Dobrar', 'Escovar', 'CSS'],
+     'status':'Em Progresso'},
+    ]
+        
+
+     #Devolve um desenvolvedor pelo ID, também consegue modificar os dados dos desenvolvedores
+@app.route('/tarefa/<int:id>/', methods=['GET', 'PUT', 'DELETE'])
+def tarefa(id):
+
+    if request.method == 'GET':
+        try:
+            response = tarefas[id]
+            print(response)
+        except IndexError:
+            mensagem = f'Registro {id} não existe'
+            response = {'status':'Falha', 'mensagem':mensagem}
+        except Exception:
+            mensagem = 'Erro Descconhecido, procure o administrador da API'
+            response = {'status':'Erro', 'mensagem':mensagem}
+        return jsonify(response)
+
+    #Altera somente o registro status, e não deixa alterar os outros!
+    elif request.method == 'PUT':
+        dadosAnteriores = tarefas[id]
+        print(dadosAnteriores)
+        dados = json.loads(request.data)
+        if (dadosAnteriores['responsavel'] != dados['responsavel'] or dadosAnteriores['id'] != dados['id'] or dadosAnteriores['tarefa'] != dados['tarefa']):
+            return jsonify({'status':'Erro', 'mensagem':'Somente é possível alterar o registro status'})
+        else:
+            tarefas[id] = dados
+            return jsonify(dados)
+
+    elif request.method == 'DELETE':
+        tarefas.pop(id)
+        return jsonify({'status':'Sucesso', 'mensagem':'Registro excluído com sucesso'})
+
+
+#Lista de todos desenvolvedores e permite incluir um novo desenvolvedor
+@app.route('/tarefa/', methods=['POST', 'GET'])
+def lista_tarefas():
+    if request.method == 'POST':
+        dados = json.loads(request.data)
+        posicao = len(tarefas)
+        dados['id'] = posicao
+        tarefas.append(dados)
+        return jsonify(tarefas[posicao])
+    elif request.method == 'GET':
+         return jsonify(tarefas)
+
